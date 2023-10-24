ddd9ef27e18e03669c4583b502615445fc9bf3fe
Matheus Henrique <matheus.hpp135@gmail.com>
2023-10-24 10:49:22 -0300

    refactor: is empty validation case for peek

--- Stack/Array/stack.h
+++ Stack/Array/stack.h
@@ -10,7 +10,7 @@ struct stack {
 
 void push(struct stack *stack, int data);
 int pop(struct stack *stack);
-bool isEmpty(struct stack *stack);
-bool isFull(struct stack *stack);
+bool is_empty(struct stack *stack);
+bool is_full(struct stack *stack);
 int peek(struct stack *stack);
 void print(struct stack *stack);
